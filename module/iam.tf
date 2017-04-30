1eb47b8350542637c172c393ae35b23aff833f22
Naoto Yokoyama <builtinnya@gmail.com>
2017-04-30 14:23:34 +0900

    :sparkles: Enable CloudWatch logging

--- module/iam.tf
+++ module/iam.tf
@@ -6,3 +6,13 @@ resource "aws_iam_role" "lambda_sns_to_slack" {
   name = "lambda-sns-to-slack"
   assume_role_policy = "${file("${path.module}/policies/lambda-assume-role.json")}"
 }
+
+#####
+# IAM policies
+#
+
+resource "aws_iam_role_policy" "lambda_sns_to_slack" {
+  name = "lambda-sns-to-slack-policy"
+  role = "${aws_iam_role.lambda_sns_to_slack.id}"
+  policy = "${file("${path.module}/policies/lambda-role-policy.json")}"
+}
